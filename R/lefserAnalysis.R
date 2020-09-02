85aaa9f8b336b55a3cfdc69ee2876a0c7b044945
Asya Khleborodova <asyakhleb@gmail.com>
2020-09-02 17:11:24 -0400

    some code alterations and comments added

--- R/lefserAnalysis.R
+++ R/lefserAnalysis.R
@@ -24,28 +24,38 @@ lefserAnalysis <- function (expr)
                 ")"))
   group <- factor(grp)
   block <- factor(blk)
+
+  # extracts p-values from Kruskal-Wallis Rank Sum Test
   kruskal.test.alt <- function(x){
     kruskal.test(x ~ group)$p.value
   }
+
+  # applies "kruskal.test.alt" function to each row (feature) of expr
+  # to detect differential abundance between classes, 0 and 1
   kw.res <- apply(expr, 1, kruskal.test.alt)
+
+  # selects p-values less than or equal to 0.05
   kw.sub <- kw.res <= 0.05
 
+  # eliminates NAs
   kw.sub[is.na(kw.sub)] <- FALSE
 
+  # extracts features with statistically significant differential abundance
+  # from "expr" matrix
   expr1 <- expr[kw.sub,]
 
-  wilcox_test_alt <- function(x, group){
-    wilcox_test(x ~ group, distribution = "exact")
-  }
-
+  # extracts p-values from Wilcoxon rank-sum test
   wilcox_test_pvalue <- function(x, group){
     pvalue(wilcox_test(x ~ group))
   }
 
+  # extracts z-statistic from Wilcoxon rank-sum test
   wilcox_test_z <- function(x, group){
     statistic(wilcox_test(x ~ group))
   }
 
+  # creates a list of boolean vectors, each vector indicates
+  # existance (TRUE) or absence (FALSE) of a class/sub-class combination
   logical.list.of.subclasses <- list()
   for (i in seq_along(unique(group))) {
     for (j in seq_along(unique(block))){
@@ -54,12 +64,16 @@ lefserAnalysis <- function (expr)
     }
   }
 
-
+  # creates empty matrices that will be filled by p-values and z-statistics from Wilcoxon rank-sum test
   pval_mat <- matrix(0, nrow(expr1), length(unique(block))^length(unique(group)))
   z_mat <- matrix(0, nrow(expr1), length(unique(block))^length(unique(group)))
 
   rownames(pval_mat) <- rownames(expr1)
   rownames(z_mat) <- rownames(expr1)
+
+  # uses Wilcoxon rank-sum test to test for significant differential abundances between
+  # subclasses of one class against subclasses of all othe classes; results are saved in
+  # "pval_mat" and "z_mat" matrices
   c <- 0
   for (i in seq_along(unique(block))){
     for (j in seq_along(unique(block))){
@@ -74,18 +88,30 @@ lefserAnalysis <- function (expr)
     }
   }
 
+  # number of p-values per feature
   num_comp <- length(unique(block))^length(unique(group))
+
+  # converts "pval_mat" into boolean matrix "logical_pval_mat" where p-values<=0.05
   logical_pval_mat <- pval_mat<=0.05
+
+  # determines which rows (features) have all p-values<=0.05
+  # and selects such rows from the matrix of z-statistics
   sub <- which(rowSums(logical_pval_mat)==num_comp)
   z_mat_sub <- z_mat[sub,]
 
+  # confirms that z-statistics of a row all have the same sign
   sub <- abs(rowSums(z_mat_sub))==abs(rowSums(abs(z_mat_sub)))
-  expr1_sub <- expr1[names(sub), ]
+  expr1_sub <- expr1[names(sub[sub==TRUE]), ]
 
+  # transposes matrix and add a "class" (i.e., group) column
+  # matrix converted to dataframe
   expr1_sub_t <- t(expr1_sub)
   expr1_sub_t <- cbind(expr1_sub_t, class = (as.numeric(group)-1))
   expr1_sub_t_df <- data.frame(expr1_sub_t)
 
+  # makes sure that more than half of the values in each for each feature are unique
+  # if that is not the case then a count value is altered by adding it to a small value
+  # generated via normal distribution with mean=0 and sd=5% of the count value
   df <- expr1_sub_t_df
   for (i in seq_along(c(1:(ncol(df)-1)))){
     for (j in seq_along(groups)){
@@ -103,29 +129,36 @@ lefserAnalysis <- function (expr)
   }
 
   expr1_sub_t_df <- df
-
+  # number of samples (i.e., subjects) in the dataframe
   lfk <- nrow(expr1_sub_t_df)
+  # rfk is the number of subject that will be used in linear discriminant analysis
   rfk <- floor(lfk*2/3)
+  # number of classes (typically two)
   ncl <- length(groups)
+  # count samples in each class of the dataframe, select the number from the class with a smaller
+  # count of samples and multiply that number by 2/*2/3*0.5
   min_cl <- as.integer(min(table(expr1_sub_t_df$class))*2/3*2/3*0.5)
+  # if min_cl is less than 1, then make it equal to 1
   min_cl <- max(min_cl,1)
-  pairs <- combn(groups,2)
+
 
   contast_within_classes_or_few_per_class <- function(expr1_sub_t_df, rand_s, min_cl, ncl){
     cols <- expr1_sub_t_df[rand_s,]
     cls <- expr1_sub_t_df$class[rand_s]
+    # if the number of classes is less than the actual number (typically two)
+    # of classes in the dataframe then return TRUE
     if (length(unique(cls)) < ncl){
       return (TRUE)
     }
-    # detect if for each class there are fewer than the minimum (min_cl) number of samples
+    # detect if for each class there are not fewer than the minimum (min_cl) number of samples
     if (TRUE %in% c(table(cls) < min_cl)){
       return (TRUE)
     }
-    # separate the randomly selected samples with replacement (cols) into a list of the two classes
+    # separate the randomly selected samples (cols) into a list of the two classes
     drops <- c("class")
     by_class <- lapply(seq_along(groups), function(x){cols[cols[, "class"]==groups[x],!(names(cols) %in% drops)]})
 
-    # within each class, count unique "counts" for each feature (i.e., microorganism, column) to make sure each has no less than min_cl
+    # makes sure that within each class all features have at least min_cl unique count values
     for (i in seq_along(groups)){
       unique_counts_per_microb = apply(by_class[[i]],2, function(x){length(unique(x))})
       if((TRUE %in% c(unique_counts_per_microb <= min_cl) & min_cl > 1) | (min_cl==1 & (TRUE %in% c(unique_counts_per_microb <= 1)))){
@@ -137,22 +170,31 @@ lefserAnalysis <- function (expr)
   }
 
   lda_fn <- function (data){
-    # test 1000 samples for contrast within classes per feature and there is at least a minimum number of samples per class
+    # test 1000 samples for contrast within classes per feature
+    # and that there is at least a minimum number of samples per class
     for (j in seq_along(1:1000)){
       rand_s <- sample(c(1:lfk),rfk, replace=TRUE)
       if(!contast_within_classes_or_few_per_class(data, rand_s, min_cl, ncl)){
         break
       }
     }
+    # lda with rfk number of samples
     lda.fit <- lda(class ~., data = data, subset = rand_s)
+    # coefficients that transform observations to discriminants
     w <- lda.fit$scaling[,1]
+    # scaling of lda coefficients
     w.unit <- w/sqrt(sum(w^2))
     sub_d <- data[rand_s,]
     ss <- sub_d[,-match("class",colnames(sub_d))]
     xy.matrix <- as.matrix(ss)
+    # the original matrix is transformed
     LD <- xy.matrix%*%w.unit
+    # effect size is calculated as difference between averaged disciminants
+    # of two classes
     effect_size <- abs(mean(LD[sub_d[,"class"]==0])-mean(LD[sub_d[,"class"]==1]))
+    # scaling lda coefficients by the efect size
     scal <- w.unit * effect_size
+    # mean count values per fclass per feature
     rres <- lda.fit$means
     rowns <- rownames(rres)
     lenc <- length(colnames(rres))
@@ -166,25 +208,33 @@ lefserAnalysis <- function (expr)
       }
 
     }
-
+    # count value differences between means of two classes for each feature
     lda.means.diff <- (lda.fit$means[2,]-lda.fit$means[1,])
+    # difference between a feature's class means and effect size adjusted lda coefficient
+    # are averaged for each feature
     (lda.means.diff+coeff)/2
   }
 
+  # lda_fn repeated 30 times, producing a matrix of 30 scores per feature
   eff_size_mat <- replicate(30, lda_fn(expr1_sub_t_df), simplify=T)
 
+  # mean of 30 scores per feature
   raw_lda_scores <- rowMeans(eff_size_mat)
 
+  # processing of score
   processed_scores <- sign(raw_lda_scores)*log((1+abs(raw_lda_scores)),10)
 
+  # sorting of scores
   processed_sorted_scores <- sort(processed_scores)
 
+  # extracting the most specific taxonomic rank of an organism
   Names <- vector("character", length(processed_sorted_scores))
   for (i in seq_along(processed_sorted_scores)){
     vec_of_strings = unlist(strsplit(names(processed_sorted_scores)[i], "[.]"))
     Names[i] = vec_of_strings[length(vec_of_strings)]
   }
 
+  # return a dataframe of taxon name and effect size score
   scores <- as.vector(processed_sorted_scores)
   return(scores_df <- data.frame(Names, scores))
 }
