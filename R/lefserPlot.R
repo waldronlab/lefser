56762ff04b646b4233974a111187ed49d33978fc
Levi Waldron <lwaldron.research@gmail.com>
2020-09-07 00:04:14 +0200

    ggplot2 to suggests, code formatting

--- R/lefserPlot.R
+++ R/lefserPlot.R
@@ -11,31 +11,33 @@
 #' @examples
 #' # an example
 lefserPlot <- function(scores_df) {
+  if (!require(ggplot2))
+    stop("Install ggplot2 to use the lefserPlot function")
   group <- ifelse(scores_df$scores > 0, 1, 0)
   scores_df$group <- as.factor(group)
-  return(
+  p <-
     ggplot(scores_df, aes(reorder(Names, scores), scores)) + ylab("LDA SCORE (log 10)") +
-      theme(
-        axis.title.y = element_blank(),
-        axis.title.x = element_text(size = 11, face = "bold"),
-        axis.text.y  = element_text(
-          vjust = 0.7,
-          size = 9,
-          face = "bold"
-        ),
-        axis.text.x  = element_text(
-          vjust = 0.7,
-          size = 9,
-          face = "bold"
-        ),
-        plot.title = element_text(
-          hjust = 0.5,
-          size = 13,
-          face = "bold"
-        )
-      ) +
-      geom_bar(stat = "identity", aes(fill = group)) +
-      scale_fill_manual(values = c("red", "forestgreen")) +
-      coord_flip()
-  )
+    theme(
+      axis.title.y = element_blank(),
+      axis.title.x = element_text(size = 11, face = "bold"),
+      axis.text.y  = element_text(
+        vjust = 0.7,
+        size = 9,
+        face = "bold"
+      ),
+      axis.text.x  = element_text(
+        vjust = 0.7,
+        size = 9,
+        face = "bold"
+      ),
+      plot.title = element_text(
+        hjust = 0.5,
+        size = 13,
+        face = "bold"
+      )
+    ) +
+    geom_bar(stat = "identity", aes(fill = group)) +
+    scale_fill_manual(values = c("red", "forestgreen")) +
+    coord_flip()
+  return(p)
 }
