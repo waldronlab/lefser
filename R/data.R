8919c1f591315e0c7d7685f2aead1bdd509abf75
Levi Waldron <lwaldron.research@gmail.com>
2020-09-07 12:30:17 +0200

    add example dataset

---
+++ R/data.R
@@ -0,0 +1,13 @@
+#' Example dataset for lefser
+#'
+#' The ZellerG_2014 dataset contains microbiome count data for CRC patients and controls.
+#' It was for curatedMetagenomicData using the script in the package directory "data/raw".
+#'
+#' @format An ExpressionSet with 1585 features, 199 samples
+#' \describe{
+#'   \item{study_condition}{adenoma, control, CRC}
+#'   \item{age_category}{adult, senoir}
+#'   ...
+#' }
+#' @source \url{https://pubmed.ncbi.nlm.nih.gov/25432777/}
+"exampledata"
---
+++ data-raw/DATASET.R
@@ -0,0 +1,10 @@
+## code to prepare example dataset goes here
+## can change dataset by replacing ZellerG_2014 with any other dataset
+
+library(curatedMetagenomicData)
+exampledata <-
+  curatedMetagenomicData("ZellerG_2014.metaphlan_bugs_list.stool",
+                         counts = TRUE,
+                         dryrun = FALSE)[[1]]
+
+usethis::use_data(exampledata, overwrite = TRUE)
