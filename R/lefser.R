28457e9bf9e42fcbb9f7534966e9f0097a7d0b1a
LiNk-NY <mramos09@gmail.com>
2020-10-16 17:43:36 -0400

    avoid using nested for loops and iterate over combinations

--- R/lefser.R
+++ R/lefser.R
@@ -25,75 +25,50 @@ fillPmatZmat <- function(group,
                          expr_sub,
                          wilcoxon.threshold)
 {
-  # extracts p-values from Wilcoxon rank-sum test
-  wilcox_test_pvalue <- function(x, group) {
-    pvalue(wilcox_test(x ~ group))
-  }
-  
-  # extracts z-statistic from Wilcoxon rank-sum test
-  wilcox_test_z <- function(x, group) {
-    statistic(wilcox_test(x ~ group))
-  }
   # creates a list of boolean vectors, each vector indicates
   # existance (TRUE) or absence (FALSE) of a class/sub-class combination
-  logical.list.of.subclasses <- list()
-  for (i in seq_along(unique(group))) {
-    for (j in seq_along(unique(block))) {
-      logical.list.of.subclasses <- 
-        append(logical.list.of.subclasses,
-               list(group == sort(unique(group))[i] &
-                      block == sort(unique(block))[j]))
-    }
-  }
-  
-  ## creates empty matrices that will be filled by p-values and z-statistics from 
-  ## Wilcoxon rank-sum test
-  pval_mat <-
-    matrix(0, nrow(expr_sub), length(unique(block)) ^ length(unique(group)))
-  z_mat <-
-    matrix(0, nrow(expr_sub), length(unique(block)) ^ length(unique(group)))
-  
-  rownames(pval_mat) <- rownames(expr_sub)
-  rownames(z_mat) <- rownames(expr_sub)
-  
+  combos <- apply(
+    expand.grid(levels(group), levels(block)), 1L, paste0, collapse = "")
+  combined <- paste0(as.character(group), as.character(block))
+  logilist <- lapply(setNames(nm = sort(combos)), `==`, combined)
+
+
   ## uses Wilcoxon rank-sum test to test for significant differential abundances between
   ## subclasses of one class against subclasses of all othe classes; results are saved in
   ## "pval_mat" and "z_mat" matrices
-  c <- 0
-  for (i in seq_along(unique(block))) {
-    for (j in seq_along(unique(block))) {
-      j <- j + length(unique(block))
-      mat = cbind(expr_sub[, logical.list.of.subclasses[[i]]],
-                  expr_sub[, logical.list.of.subclasses[[j]]])
-      group_for_mat = factor(c(rep(
-        0, sum(logical.list.of.subclasses[[i]])
-      ),
-      rep(
-        1, sum(logical.list.of.subclasses[[j]])
-      )))
-      pval_mat[, c + 1] <-
-        suppressWarnings(apply(mat, 1, wilcox_test_pvalue, group = group_for_mat))
-      z_mat[, c + 1] <-
-        suppressWarnings(apply(mat, 1, wilcox_test_z, group = group_for_mat))
-      c = c + 1
-    }
-  }
-  
-  ## number of p-values per feature
-  num_comp <- length(unique(block)) ^ length(unique(group))
-  
-  ## converts "pval_mat" into boolean matrix "logical_pval_mat" where 
+  whichlist <- lapply(logilist, which)
+  sblock <- seq_along(levels(block))
+  texp_sub <- t(expr_sub)
+  iters <- expand.grid(sblock, sblock + length(sblock))
+  # mats <- apply(iters, 1L, function(x) expr_sub[, unlist(whichlist[x]), drop = FALSE])
+  group_formats <- apply(iters, 1L, function(x) {
+    ind <- unlist(whichlist[x])
+    apply(texp_sub, 2L, function(g) {
+      wx <- wilcox_test(g ~ group, subset = ind)
+        cbind.data.frame(p.value = pvalue(wx), statistic = statistic(wx))
+    })
+  })
+
+  res <- lapply(group_formats, function(x) do.call(rbind, x))
+  pval_mat <- do.call(cbind, lapply(res, `[[`, "p.value"))
+  z_mat <- do.call(cbind, lapply(res, `[[`, "statistic"))
+
+  rownames(pval_mat) <- rownames(expr_sub)
+  rownames(z_mat) <- rownames(expr_sub)
+
+  ## converts "pval_mat" into boolean matrix "logical_pval_mat" where
   ## p-values <= wilcoxon.threshold
   logical_pval_mat <- pval_mat <= wilcoxon.threshold
-  
+  logical_pval_mat[is.na(logical_pval_mat)] <- FALSE
+
   ## determines which rows (features) have all p-values<=0.05
   ## and selects such rows from the matrix of z-statistics
-  sub <- which(rowSums(logical_pval_mat) == num_comp)
+  sub <- apply(logical_pval_mat, 1L, all)
   z_mat_sub <- z_mat[sub,]
-  
+
   # confirms that z-statistics of a row all have the same sign
-  sub <- abs(rowSums(z_mat_sub)) == abs(rowSums(abs(z_mat_sub)))
-  expr_sub <- expr_sub[names(sub[sub]), ]
+  sub <- rowSums(z_mat_sub) == rowSums(abs(z_mat_sub))
+  expr_sub[names(sub[sub]), ]
 }
 
 ## ensures that more than half of the values in each for each feature are unique
