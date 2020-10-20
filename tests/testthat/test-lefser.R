96e3c8d9d519d9c08337b3c830ae6f7c58ff71fa
LiNk-NY <marcel.ramosperez@roswellpark.org>
2020-10-20 17:31:46 -0400

    update unit tests

--- tests/testthat/test-lefser.R
+++ tests/testthat/test-lefser.R
@@ -1,32 +1,44 @@
+checkEnding <- function(resObj, index, column, value) {
+    endsWith(resObj[index, column], value)
+}
+
 test_that("lefser and lefserPlot work", {
-  data("zeller14")
+  dataenv <- new.env(parent = emptyenv())
+  data("zeller14", package = "lefser", envir = dataenv)
+  zeller14 <- dataenv[["zeller14"]]
+
   zellersub <- zeller14[1:150, zeller14$study_condition != "adenoma"]
-  expect_error(lefser(zellersub, groupCol = NULL, blockCol = NULL),
-               "Group assignment 'groupCol' must be specified")
+  ## subsetting a DataFrame with NULL
+  expect_error(lefser(zellersub, groupCol = NULL, blockCol = NULL))
   set.seed(1)
   results <- lefser(zellersub, groupCol = "study_condition", blockCol = NULL)
   expect_equal(colnames(results), c("Names", "scores"))
-  expect_equal(results[1, "Names"], "p__Firmicutes")
-  expect_equal(results[nrow(results), "Names"], "o__Bacteroidales")
+  expect_true(checkEnding(results, 1, "Names", "p__Firmicutes`"))
+  expect_true(
+    checkEnding(results, nrow(results), "Names", "o__Bacteroidales`")
+  )
   expect_equal(results[1, "scores"],-6.431365, tolerance = 0.2)
   expect_equal(results[nrow(results), "scores"], 6.402655, tolerance = 0.2)
-  results2 <- lefser(zellersub, groupCol = "study_condition", blockCol = "age_category")
-  
+  set.seed(1)
+  results2 <- lefser(
+    zellersub, groupCol = "study_condition", blockCol = "age_category"
+  )
   expect_equal(colnames(results2), c("Names", "scores"))
-  expect_equal(results2[1, "Names"], "o__Lactobacillales")
-  expect_equal(results2[nrow(results2), "Names"], "s__Eubacterium_hallii")
+  expect_true(endsWith(results2[1, "Names"], "o__Lactobacillales`"))
+  expect_true(endsWith(
+    results2[nrow(results2), "Names"], "s__Eubacterium_hallii`"
+  ))
   expect_equal(results2[1, "scores"],-5.792313, tolerance = 0.2)
   expect_equal(results2[nrow(results2), "scores"], 5.20564, tolerance = 0.2)
 
   expect_equal(nrow(results2), 3)
-  expect_equal(
-    results2$Names,
-    c(
-      "o__Lactobacillales",
-      "s__Ruminococcus_sp_5_1_39BFAA",
-      "s__Eubacterium_hallii"
-    )
-  )
+  expect_true(all(
+    mapply(endsWith, results2$Names, c(
+      "o__Lactobacillales`",
+      "s__Ruminococcus_sp_5_1_39BFAA`",
+      "s__Eubacterium_hallii`"
+    ))
+  ))
   expect_equal(results2$scores,
                c(-5.792313,-5.079186, 5.205640),
                tolerance = 0.2)
