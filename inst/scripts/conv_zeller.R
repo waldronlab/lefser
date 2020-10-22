59f8b215dd9b6651aa7b00ba5a5c445017fc1715
LiNk-NY <mramos09@gmail.com>
2020-10-22 15:24:54 -0400

    update script for comparison

--- inst/scripts/conv_zeller.R
+++ inst/scripts/conv_zeller.R
@@ -1,6 +1,7 @@
 library(lefser)
 
 zell <- zeller14[, zeller14$study_condition != "adenoma"]
+nr <- nrow(zell)
 exprs <- assay(zell, i = 1L)
 datas <- data.frame(
     condition = lefser:::.numeric01(zell$study_condition),
@@ -15,8 +16,17 @@ text <- mapply(function(x, y) {
 )
 
 ## file for http://huttenhower.sph.harvard.edu/galaxy/ LEfSe
-writeLines(text, con = file("~/data/lefser/setupdata.txt"))
+# writeLines(text, con = file("~/data/lefser/zelle14_150.txt"))
+writeLines(text,
+    con = file(paste0("~/data/lefser/zelle14_", nr, ".txt")))
 
 ## compared to results from lefser
+loc <- Sys.getlocale("LC_COLLATE")
+Sys.setlocale("LC_COLLATE", "C")
+on.exit(Sys.setlocal("LC_COLLATE", loc))
+
+# seed from
+# https://github.com/SegataLab/lefse/blob/a4b3140c34b3abe5579a916a20c2a8659c3ac53c/lefse.py#L9
+set.seed(1982)
 res <- lefser(zell, groupCol = "study_condition", blockCol = "age_category")
 lefserPlot(res)
