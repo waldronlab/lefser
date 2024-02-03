60ed7d1385f9c3ecff31d71dc7b96db729dec0b2
Sandy <peacesandy04@gmail.com>
2024-02-03 06:15:18 -0800

    append and created a testthat unit test of the fix

--- R/utils.R
+++ R/utils.R
@@ -4,9 +4,9 @@
 #' The function calculates the column totals and divides each value within the
 #' column by the respective column total.
 #'
-#' @inheritParams lefser
-#'
 #' @param se A SummarizedExperiment object with counts
+#'
+#' @return A modified SummarizedExperiment object
 #'
 #' @examples
 #'
@@ -22,14 +22,57 @@
 #'
 #' @export
 relativeAb <- function(se, assay = 1L) {
-    assay_data <- assay(se, i = assay)
-    csums <- colSums(assay_data)
-    div <- matrix(rep(csums, each = nrow(assay_data)), ncol = ncol(assay_data))
-    res <- assay_data / div
-    assaylist <- assays(se)
-    newalist <- append(
-        assaylist, values = S4Vectors::SimpleList(rel_abs = res), after = 0L
-    )
-    assays(se) <- newalist
-    se
+  
+  assay_data <- assay(se, i = assay)
+  
+  
+  csums <- colSums(assay_data)
+  
+  
+  cat("")
+  print(assay_data)
+  
+  
+  res <- assay_data / csums * 1e6
+  
+  
+  cat("")
+  print(res)
+  
+  
+  assaylist <- assays(se)
+  for (i in seq_along(assaylist)) {
+    assaylist[[i]] <- assaylist[[i]] * 1e6
+  }
+  
+  
+  newalist <- append(
+    assaylist, 
+    values = S4Vectors::SimpleList(rel_abs = res), 
+    after = 0L
+  )
+  
+  
+  assays(se) <- newalist
+  
+  
+  cat("")
+  print(se)
+  
+  
+  return(se)
 }
+
+
+se <- SummarizedExperiment(
+  assays = list(
+    counts = matrix(
+      rep(1, 4), ncol = 1, dimnames = list(LETTERS[1:4], "SAMP")
+    )
+  )
+)
+
+assay(se)
+assay(relativeAb(se))
+
+
--- tests/testthat.R
+++ tests/testthat.R
@@ -2,3 +2,13 @@ library(testthat)
 library(lefser)
 
 test_check("lefser")
+
+data(zeller14)
+
+zeller14sub <- zeller14[, zeller14$study_condition != "adenoma"]
+
+zeller14ra <- lefser::relativeAb(zeller14sub)
+
+
+expect_equal(assay(zeller14ra)$rel_abs[1, 1], assay(zeller14ra)$counts[1, 1] * 1e6)
+
