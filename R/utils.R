88bd310c4743a5d98134b0f934ee23cf7a636a7c
Sandy <peacesandy04@gmail.com>
2024-02-13 01:41:56 -0800

    Append and created a testthat unit test of the fix

--- R/utils.R
+++ R/utils.R
@@ -1,12 +1,16 @@
 #' Utility function to calculate relative abundances
 #'
+#'
+#'
 #' @description
 #' The function calculates the column totals and divides each value within the
 #' column by the respective column total.
+#' 
+#' 
+#'
+#' @inheritParams lefser
 #'
 #' @param se A SummarizedExperiment object with counts
-#'
-#' @return A modified SummarizedExperiment object
 #'
 #' @examples
 #'
@@ -19,60 +23,29 @@
 #' )
 #' assay(se)
 #' assay(relativeAb(se))
+#' 
+#' 
+#' utility function for relativeAb to return counts per million
+#'
+#' @description
+#' This function calculates the relative abundance of each feature in a dataset,
+#' expressed as counts per million (CPM) 
+#' 
+#' @param normalize Logical value indicating whether counts should be normalized
+#' to counts per million (default is TRUE)
+#' 
+#' 
 #'
 #' @export
 relativeAb <- function(se, assay = 1L) {
-  
   assay_data <- assay(se, i = assay)
-  
-  
   csums <- colSums(assay_data)
-  
-  
-  cat("")
-  print(assay_data)
-  
-  
-  res <- assay_data / csums * 1e6
-  
-  
-  cat("")
-  print(res)
-  
-  
+  div <- matrix(rep(csums, each = nrow(assay_data)), ncol = ncol(assay_data))
+  res <- assay_data / div * 1e6
   assaylist <- assays(se)
-  for (i in seq_along(assaylist)) {
-    assaylist[[i]] <- assaylist[[i]] * 1e6
-  }
-  
-  
   newalist <- append(
-    assaylist, 
-    values = S4Vectors::SimpleList(rel_abs = res), 
-    after = 0L
+    assaylist, values = S4Vectors::SimpleList(rel_abs = res), after = 0L
   )
-  
-  
   assays(se) <- newalist
-  
-  
-  cat("")
-  print(se)
-  
-  
-  return(se)
+  se
 }
-
-
-se <- SummarizedExperiment(
-  assays = list(
-    counts = matrix(
-      rep(1, 4), ncol = 1, dimnames = list(LETTERS[1:4], "SAMP")
-    )
-  )
-)
-
-assay(se)
-assay(relativeAb(se))
-
-
--- tests/testthat.R
+++ tests/testthat.R
@@ -1,6 +1,8 @@
 library(testthat)
 library(lefser)
 
+  
+
 test_check("lefser")
 
 data(zeller14)
@@ -10,5 +12,8 @@ zeller14sub <- zeller14[, zeller14$study_condition != "adenoma"]
 zeller14ra <- lefser::relativeAb(zeller14sub)
 
 
-expect_equal(assay(zeller14ra)$rel_abs[1, 1], assay(zeller14ra)$counts[1, 1] * 1e6)
-
+expect_warning(lefser(zeller14sub, groupCol = "study_condition"), regexp = "^Convert counts.+")
+expect_equal(assayNames(zeller14ra), c("rel_abs", "exprs"))
+expect_equal(colSums(assay(zeller14ra, "rel_abs")), rep(1e6, ncol(zeller14ra)), check.attributes = FALSE)
+set.seed(1)
+expect_no_warning(x <- lefser(zeller14ra, groupCol = "study_condition"))
