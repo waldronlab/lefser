4723414d372f33e0712ea6573a32dda085df6aaf
Sandy <peacesandy04@gmail.com>
2024-02-14 18:51:39 -0800

    fixed the documentation and deleted the r.data and r.history files

--- R/utils.R
+++ R/utils.R
@@ -1,12 +1,8 @@
 #' Utility function to calculate relative abundances
 #'
-#'
-#'
 #' @description
 #' The function calculates the column totals and divides each value within the
 #' column by the respective column total.
-#' 
-#' 
 #'
 #' @inheritParams lefser
 #'
@@ -24,18 +20,15 @@
 #' assay(se)
 #' assay(relativeAb(se))
 #' 
-#' 
 #' utility function for relativeAb to return counts per million
 #'
 #' @description
-#' This function calculates the relative abundance of each feature in a dataset,
-#' expressed as counts per million (CPM) 
+#' This function calculates the relative abundance of each feature in the SummarizedExperiment 
+#' object containing count data, expressed as counts per million (CPM) 
 #' 
-#' @param Logical value indicating whether counts should be normalized
-#' to counts per million (default is TRUE)
+#' @param se returns a new SummarizedExperiment object with counts per million
+#' calculated and added as a new assay named rel_abs.
 #' 
-#' 
-#'
 #' @export
 relativeAb <- function(se, assay = 1L) {
   assay_data <- assay(se, i = assay)
