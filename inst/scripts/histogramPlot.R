5fc33adc82a03bed8654e1b4514d507305ce96df
sdgamboa <samuel.gamboa.tuz@gmail.com>
2024-09-08 18:14:05 -0400

    Update draft code for plot

--- inst/scripts/histogramPlot.R
+++ inst/scripts/histogramPlot.R
@@ -1,6 +1,8 @@
 # library(lefser)
 library(ggplot2)
 library(dplyr)
+library(ggpubr)
+library(purrr)
 data(zeller14)
 zeller14 <- zeller14[, zeller14$study_condition != "adenoma"]
 tn <- get_terminal_nodes(rownames(zeller14))
@@ -15,14 +17,57 @@ res <- lefser(
 
 dat <- .prepareDataHistogram(res)
 
-bacName <- "k__Bacteria|p__Firmicutes|c__Clostridia|o__Clostridiales|f__Ruminococcaceae|g__Ruminococcus|s__Ruminococcus_sp_5_1_39BFAA|t__GCF_000159975"
-dat |> 
+dat2 <- dat |> 
     filter(features == bacName) |> 
     arrange(desc(study_condition), desc(age_category), desc(abundance)) |> 
     mutate(sample = forcats::fct_inorder(sample)) |> 
+    mutate(classes = paste0(study_condition, "--", age_category))
+myClasses <- unique(dat2$classes)
+
+vals <- map(myClasses, ~ {
+    subDat2 <- dat2 |> 
+        filter(classes == .x)
+    sumDat <- summarise(subDat2, mean = mean(abundance))
+    meanValue <- pull(sumDat, mean)
+    sampleValue <- pull(subDat2, sample)
+    min = head(sampleValue, 1)
+    max = tail(sampleValue, 1)
+    data.frame(x1 = min, x2 = max, y1 = meanValue, y2 = meanValue)
+} )
+
+
+
+x <- split(dat2, dat2$classes) |> 
+    map(~ pull(.x, sample))
+
+unlist(vals[[2]][1:2]) %in% x[[1]]
+
+
+dat2 |> 
+    filter(features == bacName) |> 
     ggplot(aes(sample, abundance)) +
-    geom_col(aes(fill = age_category)) +
-    facet_wrap(~ study_condition, scales = "free_x") +
+    geom_col(aes(fill = age_category), width = 1) +
+    geom_segment(
+        data = vals[[1]], mapping = aes(x = x1, xend = x2, y = y1, yend = y2)
+    ) +
+    geom_segment(
+        data = vals[[2]], mapping = aes(x = x1, xend = x2, y = y1, yend = y2)
+    ) +
+    geom_segment(
+        data = vals[[3]], mapping = aes(x = x1, xend = x2, y = y1, yend = y2)
+    ) +
+    geom_segment(
+        data = vals[[4]], mapping = aes(x = x1, xend = x2, y = y1, yend = y2),
+    ) +
+    annotate(
+        "label", x = vals[[1]]$x1, y=max(dat2$abundance), label = "Control",
+        hjust = 0, vjust = 1 
+    ) +
+    annotate(
+        "label", x = vals[[3]]$x1, y=max(dat2$abundance), label = "CRC",
+        hjust = 0, vjust = 1
+    ) +
+    # facet_wrap(~ study_condition, scales = "free_x", nrow = 1) +
     theme_bw() +
     scale_y_continuous(
         labels = scales::scientific, expand = c(0, 0)
@@ -35,3 +80,4 @@ dat |>
         axis.ticks.x = element_blank(),
         panel.grid.major.x = element_blank()
     )
+
