7950522a50b4c458330d34c4755c8e49dc2fd711
sdgamboa <samuel.gamboa.tuz@gmail.com>
2024-09-09 17:10:21 -0400

    Add stringr to imports. Use str_wrap to wrap long titles in plots. Update documentation.

--- R/lefsePlotHistogram.R
+++ R/lefsePlotHistogram.R
@@ -1,40 +1,40 @@
-#' Plot Features
-#' 
+#' Plot Feature
+#'
 #' \code{lefsePlotFeat} plots the abundance data of a DA feature across all
 #' samples.
 #'
-#' @param res An object of class lefser_df, 
+#' @param res An object of class lefser_df,
 #' output of the \code{lefser} function.
 #' @param fName A character string. The name of a feature in the lefser_df
 #' object
-#' 
+#'
 #' @details
 #' The solid lines represent the mean by group or by group+block
-#' (if the block variable is present).  
+#' (if the block variable is present).
 #' The dashed lines represent the median by group or by group+block
 #' (if the block variable is present).
-#' 
+#'
 #' @return A ggplot object.
 #' @export
 #'
 #' @examples
-#' 
+#'
 #' data(zeller14)
 #' zeller14 <- zeller14[, zeller14$study_condition != "adenoma"]
 #' tn <- get_terminal_nodes(rownames(zeller14))
 #' zeller14tn <- zeller14[tn,]
 #' zeller14tn_ra <- relativeAb(zeller14tn)
-#' 
+#'
 #' # (1) Using classes only
-#' res_group <- lefser(zeller14tn_ra, 
+#' res_group <- lefser(zeller14tn_ra,
 #'                     groupCol = "study_condition")
 #' # (2) Using classes and sub-classes
-#' res_block <- lefser(zeller14tn_ra, 
-#'                     groupCol = "study_condition", 
+#' res_block <- lefser(zeller14tn_ra,
+#'                     groupCol = "study_condition",
 #'                     blockCol = "age_category")
 #' plot_group <- lefsePlotFeat(res_group, res_group$features[[1]])
 #' plot_block <- lefsePlotFeat(res_block, res_block$features[[2]])
-#' 
+#'
 lefsePlotFeat <- function(res, fName) {
     dat <- .prepareDataHistogram(res = res, fName = fName)
     l <- split(dat, dat$groupCol)
@@ -42,16 +42,16 @@ lefsePlotFeat <- function(res, fName) {
     vals <- purrr::map(l, ~ {
         head(.x[["sample"]], 1)
     })
-    sumDat <- .sumDatHist(dat) 
+    sumDat <- .sumDatHist(dat)
     cond <- "blockCol" %in% colnames(dat)
     if (isFALSE(cond)) {
-        p <- dat |> 
+        p <- dat |>
             ggplot2::ggplot(
                 data = dat, mapping = ggplot2::aes(sample, abundance)
             ) +
             ggplot2::geom_col(fill = "firebrick3", width = 1)
     } else if (isTRUE(cond)) {
-        p <- dat |> 
+        p <- dat |>
             ggplot2::ggplot(
                 data = dat, mapping = ggplot2::aes(sample, abundance)
             ) +
@@ -65,7 +65,7 @@ lefsePlotFeat <- function(res, fName) {
         ) +
         ggplot2::labs(
             x = "Samples", y = "Relative abundance",
-            title = fName
+            title = stringr::str_wrap(fName, whitespace_only = FALSE)
         ) +
         ggplot2::annotate(
             geom = "label",
@@ -95,7 +95,7 @@ lefsePlotFeat <- function(res, fName) {
                 mapping = ggplot2::aes(
                     x = x1, xend = x2,
                     y = mean1, yend = mean2
-                ), 
+                ),
                 linetype = 1
             ) +
             ggplot2::geom_segment(
@@ -115,7 +115,7 @@ lefsePlotFeat <- function(res, fName) {
     if (isFALSE(cond)) {
         l <- split(x, x$groupCol)
     } else if (isTRUE(cond)) {
-       l <- x |> 
+       l <- x |>
            dplyr::mutate(
                classes = paste0(.data$groupCol, "--", .data$blockCol)
            ) |>
@@ -150,35 +150,35 @@ lefsePlotFeat <- function(res, fName) {
     sampleData <- as.data.frame(SummarizedExperiment::colData(tse))
     sampleData <- sampleData[, selectCols, drop = FALSE]
     sampleData <- tibble::rownames_to_column(sampleData, var = "sample")
-    dat <- tse[res[["features"]],] |> 
-        SummarizedExperiment::assay() |> 
-        as.data.frame() |> 
-        tibble::rownames_to_column(var = "features") |> 
+    dat <- tse[res[["features"]],] |>
+        SummarizedExperiment::assay() |>
+        as.data.frame() |>
+        tibble::rownames_to_column(var = "features") |>
         tidyr::pivot_longer(
             cols = 2:tidyselect::last_col(),
             names_to = "sample",
             values_to = "abundance"
-        ) |> 
+        ) |>
         dplyr::left_join(sampleData, by = "sample")
     dat[[groupCol]] <- forcats::fct_relevel(dat[[groupCol]], refGrp)
-    
+
     if (is.null(blockCol)) {
-        dat <- dat |> 
-            dplyr::filter(.data[["features"]] == fName) |> 
+        dat <- dat |>
+            dplyr::filter(.data[["features"]] == fName) |>
             dplyr::arrange(
                 .data[[groupCol]],
                 dplyr::desc(.data[["abundance"]])
-            ) |> 
+            ) |>
             dplyr::mutate(sample = forcats::fct_inorder(.data[["sample"]]))
         colnames(dat)[which(colnames(dat) == groupCol)] <- "groupCol"
     } else if (!is.null(blockCol)) {
-        dat <- dat |> 
-            dplyr::filter(.data[["features"]] == fName) |> 
+        dat <- dat |>
+            dplyr::filter(.data[["features"]] == fName) |>
             dplyr::arrange(
                 .data[[groupCol]],
                 dplyr::desc(.data[[blockCol]]),
                 dplyr::desc(.data[["abundance"]])
-            ) |> 
+            ) |>
             dplyr::mutate(sample = forcats::fct_inorder(.data[["sample"]]))
         colnames(dat)[which(colnames(dat) == groupCol)] <- "groupCol"
         colnames(dat)[which(colnames(dat) == blockCol)] <- "blockCol"
