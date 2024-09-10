25053491a70eba89fcf9680b412ae8f4cbd98971
sdgamboa <samuel.gamboa.tuz@gmail.com>
2024-09-10 16:59:44 -0400

    Add .selectPalette function.

--- R/lefserPlot.R
+++ R/lefserPlot.R
@@ -10,11 +10,12 @@ utils::globalVariables(c("features", "scores"))
 #' @importFrom utils head tail
 #'
 #' @param df Data frame produced by \code{lefser}.
-#' @param colors A character(2). Colors corresponding to class 0 and 1. 
-#' Defaults to `c("red", "forestgreen")`.
-#' @param trim.names Under the default (`TRUE`), this function extracts the 
+#' @param colors Colors corresponding to class 0 and 1.
+#' Options: "c" (colorblind), "l" (lefse), "g" (greyscale). Defaults to "c".
+#' This argument also accepts a character(2) with two color names.
+#' @param trim.names Under the default (`TRUE`), this function extracts the
 #' most specific taxonomic rank of organism.
-#' @param title A character(1). The title of the plot. 
+#' @param title A character(1). The title of the plot.
 #' @param label.font.size A numeric(1). The font size of the feature labels.
 #' The default is `3`.
 #'
@@ -26,17 +27,18 @@ utils::globalVariables(c("features", "scores"))
 #' @examples
 #' example("lefser")
 #' lefserPlot(res_group)
-#' 
+#'
 #' @export
-lefserPlot <- function(df, 
-                       colors = c("red", "forestgreen"),
+lefserPlot <- function(df,
+                       colors = "c",
                        trim.names = TRUE,
                        title = "",
                        label.font.size = 3) {
-    
+
     df <- .trunc(df, trim.names)
     groups <- attr(df, "groups")
-  
+    colors <- .selectPalette(colors)
+
     ## Create the `group` column
      if (!is.null(groups)) {
          group <- ifelse(df$scores > 0, tail(groups, 1), head(groups, 1))
@@ -45,8 +47,8 @@ lefserPlot <- function(df,
         group <- ifelse(df$scores > 0, 1, 0)
         df$group <- as.factor(group)
     }
-  
-    ## Add the `order` column based on the scores 
+
+    ## Add the `order` column based on the scores
     ## To make duplicated features behave independently
     df <- df %>%
         arrange(scores) %>%
@@ -71,17 +73,17 @@ lefserPlot <- function(df,
             legend.key.width = unit(0.6, 'cm')) +
         scale_fill_manual(values = colors) +
         geom_text(    # Feature labeling
-            aes(y = 0, label = features), 
+            aes(y = 0, label = features),
             hjust = ifelse(df$scores < 0, 0, 1),
             nudge_y = ifelse(df$scores < 0, 0.1, -0.1),
             color = "black",
-            size = label.font.size) +    
+            size = label.font.size) +
         theme(    # Guide lines
             panel.grid.major.x = element_line(
                 color = "grey", linewidth = 0.5, linetype = "dotted"),
             panel.grid.minor.x = element_line(
                 color = "grey", linewidth = 0.5, linetype = "dotted")) +
         coord_flip()
-      
+
       return(plt)
 }
--- inst/scripts/histogramPlot.R
+++ inst/scripts/histogramPlot.R
@@ -5,6 +5,19 @@ tn <- get_terminal_nodes(rownames(zeller14))
 zeller14tn <- zeller14[tn,]
 zeller14tn_ra <- relativeAb(zeller14tn)
 
+res <- lefser(
+    zeller14tn_ra, groupCol = "study_condition", blockCol = "age_category"
+)
+lefserPlot(res)
+lefserPlot(res, colors = "l")
+lefserPlot(res, colors = "g")
+lefserPlot(res, colors = c("purple", "pink"))
+sessioninfo::session_info()
+
+
+
+
+
 # (1) Using classes only
 res_group <- lefser(zeller14tn_ra,
                     groupCol = "study_condition")
@@ -12,7 +25,18 @@ res_group <- lefser(zeller14tn_ra,
 res_block <- lefser(zeller14tn_ra,
                     groupCol = "study_condition",
                     blockCol = "age_category")
-plot_group <- lefsePlotFeat(res_group, res_group$features[[1]])
-plot_group
-plot_block <- lefsePlotFeat(res_block, res_block$features[[2]])
+
+plot_group <- lefsePlotFeat(res_group, res_group$features[[1]], colors = "g")
+plot_block <- lefsePlotFeat(res_block, res_block$features[[2]], colors = "g")
+
+plot_group <- lefsePlotFeat(res_group, res_group$features[[1]], colors = c("orange", "blue"))
+plot_block <- lefsePlotFeat(res_block, res_block$features[[1]], colors = c("orange", "blue"))
 plot_block
+
+lefserPlot(res_block, colors = "")
+
+
+
+
+
+
