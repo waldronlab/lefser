c4dceef8d2dd9a3d9758d649472c3c8c181d94fd
sdgamboa <samuel.gamboa.tuz@gmail.com>
2024-09-13 00:59:07 -0400

    Exploring plotting options with ggtree

--- R/lefsePlotClad.R
+++ R/lefsePlotClad.R
@@ -1,3 +1,37 @@
 lefsePlotClad <- function(x) {
     x
 }
+
+.pathString2DataFrame <- function(pathString) {
+    s <- stringr::str_c("ROOT|", pathString)
+    chr_vct <- stringr::str_split(s, "\\|")[[1]]
+    data.frame(
+        from = chr_vct[1:length(chr_vct)-1],
+        to = chr_vct[2:length(chr_vct)]
+    )
+}
+
+.toTree <- function(v) {
+    edgeDF <- v |> 
+        purrr::map(.pathString2DataFrame) |> 
+        dplyr::bind_rows() |> 
+        dplyr::distinct()
+    tipLabels <- stringr::str_extract(v, "[^|]+$")
+    nodeLabels <- unique(edgeDF$from)
+    idMap <- 1:(length(tipLabels) + length(nodeLabels))
+    names(idMap) <- c(tipLabels, nodeLabels)
+    edgeMat <- matrix(
+        data = c(idMap[edgeDF$from], idMap[edgeDF$to]),
+        ncol = 2
+    )
+    tr <- list(
+        edge = edgeMat,
+        tip.label = tipLabels,
+        node.label = nodeLabels,
+        Nnode = length(nodeLabels),
+        Ntip = length(tipLabels)
+    )
+    class(tr) <- "phylo"
+    tr
+    # treeio::as.treedata(tr)
+}
--- inst/scripts/cladogramPlot.R
+++ inst/scripts/cladogramPlot.R
@@ -1,19 +1,20 @@
-
+library(SummarizedExperiment)
 ## Taxonomy is encoded in the rownames
 data("zeller14")
-z14 <- zeller14
+z14 <- zeller14[, zeller14$study_condition != "adenoma"]
 tn <- get_terminal_nodes(rownames(z14))
-z14tn <- z14[tn,]
+z14tn <- z14[tn, ]
 z14tn_ra <- relativeAb(z14tn)
 
-## Let's transfer this taxonomy to the rowData
-dat <- data.frame(x  = rownames(z14tn_ra)) |>
+res <- lefser(relab = z14tn_ra, groupCol = "study_condition")
+
+taxDat <- data.frame(x  = rownames(z14tn_ra)) |>
     tidyr::separate(
         col = "x", into = paste0("col", 1:10),
         sep = "\\|", extra = "merge", fill = "right"
     ) |>
     purrr::discard(~ all(is.na(.x)))
-firstLetter <- purrr::map_chr(dat, ~ {
+firstLetter <- purrr::map_chr(taxDat, ~ {
     taxLev <- stringr::str_extract(.x, "\\w__")
     unique(taxLev[which(!is.na(taxLev))])
 })
@@ -25,86 +26,30 @@ rankNames <- dplyr::case_when(
         firstLetter == "f__" ~ "family",
         firstLetter == "g__" ~ "genus",
         firstLetter == "s__" ~ "species",
-        firstLetter == "t__" ~ "strain"
+        firstLetter == "t__" ~ "strain",
     )
 
-colnames(dat) <- rankNames
-dat$pathString <- rownames(z14tn_ra)
+colnames(taxDat) <- rankNames
+taxDat$pathString <- rownames(z14tn_ra)
 
-#
-# tr <- toTree(dat)
-# data.tree::FromDataFrameTable(dat)
-# data_tree <- as.Node(dat, pathName = "pathString", pathDelimiter = "|")
-# nw <- data.tree::ToNewick(data_tree)
-# sl <- data.tree::ToListSimple(data_tree)
+res$pathString <- res$features
+res$features <- stringr::str_extract(res$features, "[^|]+$")
+res2 <- res |> 
+    mutate(sample = ifelse(scores > 0, "CRC", "control")) |> 
+    mutate(abs = abs(scores)) 
+# eolnames(res2)[which(colnames(res2) == "features")] <- "node"
+tree <- .toTree(res2$pathString)
+res2$node <- match(res2$features, tree$tip.label)
+res2 <- dplyr::relocate(res2, node)
 
-t1 <- taxa2phylo(paste0("ROOT|", dat$pathString))
-
-ggtree(t1, layout = "circular")
-
-
-
-
-
-
-
-
-
-
-
-
-
-taxonname2edgelist <- function(taxon) {
-    # print(taxon)
-    v = strsplit(taxon,'\\|')[[1]]
-    v = v[!v=='NA']
-    if(length(v)>1) {
-        lv = length(v)
-        df = data.frame(from=v[1:(lv-1)], to = v[2:lv])
-    } else {
-        df = data.frame()
-    }
-    df
-}
-
-
-taxa2edgelist <- function(taxa) {
-    taxa_edgelist <- lapply(taxa,taxonname2edgelist)
-    df = unique(do.call(rbind, taxa_edgelist))
-    return(df)
-}
-
-taxa2phylo <- function(taxa) {
-    edgelist = taxa2edgelist(taxa)
-    edgelist = as.matrix(edgelist)
-
-    edgelist = edgelist[!is.na(edgelist[,1]) & !is.na(edgelist[,2]),]
-
-    from <- edgelist[,1]
-    to <- edgelist[,2]
-    ids <- unique(c(edgelist[,1], edgelist[,2]))
-
-    tip.label <- setdiff(ids, from)
-    node.label <- unique(from)
-
-    # make a map from taxonomy ID to internal 1:n ids
-    idmap <- 1:(length(tip.label) + length(node.label))
-    names(idmap) <- c(tip.label, node.label)
-
-    # make a phylo object
-    tree <- list(
-        edge       = matrix(c(idmap[as.character(from)], idmap[as.character(to)]), ncol=2),
-        tip.label  = unname(tip.label),
-        node.label = unname(node.label),
-        Nnode      = length(node.label)
-    )
-    class(tree) <- 'phylo'
-
-    tree
-
-}
-
-t1 <- taxa2phylo(nms)
-
-
-
+gt <- ggtree(
+    tree, layout = "circular",  branch.length = "none", size = 0.25
+) %<+% res2 
+gt +
+    geom_tiplab(aes(label = features), size = 2, geom = "label") +
+    geom_tippoint(aes(color = sample, size = abs)) +
+    theme(legend.position = "right") + 
+    scale_color_manual(
+        values = c("purple", "pink"), breaks = c("control", "CRC")
+    ) +
+    scale_size_continuous(range = c(3, 10))
