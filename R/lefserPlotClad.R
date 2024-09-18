99eecc43024ef0a41431abd9aea97dd82d3391b1
sdgamboa <samuel.gamboa.tuz@gmail.com>
2024-09-17 20:55:41 -0400

    Update lefserPlotClad function

--- R/lefserPlotClad.R
+++ R/lefserPlotClad.R
@@ -1,81 +1,127 @@
 
 # Functions for plotting a cladogram --------------------------------------
 
-#' Plot Cladogram
+#' LEfSer plot cladogram
 #'
-#' @param x An object of class "lefser_df" or "lefesr_df_all".
+#' @param df An object of class "lefser_df" or "lefesr_df_all".
+#' @param colors dfsdf
+#' @param showTipLabels sdfsd
+#' @param showNodeLabels sdfsdfs
 #'
 #' @importFrom ggtree %<+%
 #'
 #' @return A ggtree object.
 #' @export
 #'
-lefserPlotClad <- function(x) {
-    # if (!"lefser_df_all" %in% class(x)) {
-    #     stop(
-    #         "You need an object of class 'lefser_df_all'",
-    #         call. = FALSE
-    #     )
-    # }
-    tree <- attr(x, "tree")
-    control <- attr(x, "lgroupf")
-    case <- attr(x, "case")
-
+lefserPlotClad <- function(
+        df, colors = "c", showTipLabels = FALSE, showNodeLabels = "p"
+) {
+    inputClass <- class(df)[1]
+    if (inputClass == "lefser_df") {
+        message("Woriking with lefser_df. Consider using lefserAll.")
+        # df$features <- .extracTips(df$features)
+    } else if (inputClass == "lefser_df_all") {
+        message("Working with lefser_df_all")
+        ## .extractTips should be use here as well
+        ## The feature names format should use full taxonomy
+    } else {
+        stop(
+            "You need an object of class 'lefser_df_all'",
+            call. = FALSE
+        )
+    }
+    
+    df$features <- .extracTips(df$features)
+    
+    colors <- .selectPalette(colors)
+    tree <- attr(df, "tree")
+    controlVar <- attr(df, "lgroupf")
+    caseVar <- attr(df, "case")
+    
+    res <- df |>
+        dplyr::mutate(
+            sample = dplyr::case_when(
+                ## This assumes positive values always mean enriched in
+                ## the case condition.
+                .data[["scores"]] > 0 ~ .env[["caseVar"]],
+                TRUE ~ .env[["controlVar"]]
+            )
+        ) |>
+        dplyr::mutate(abs = abs(.data[["scores"]])) |>
+        as.data.frame()
+    
     labels <- c(tree$tip.label, tree$node.label)
-    x$node <- match(x$features, labels)
-    dat <- dplyr::relocate(x, node)
+    res$node <- match(res$features, labels)
+    dat <- dplyr::relocate(res, node)
+    
     internalNodes <- ape::Ntip(tree) + 1:ape::Nnode(tree)
     collapseThem <- purrr::map_int(internalNodes, ~ {
         chNods <- treeio::offspring(.data = tree, .node = .x, type = "tips")
-        if (any(chNods %in% x$node)) {
+        if (any(chNods %in% dat$node)) {
             return(NA)
         } else {
             return(.x)
         }
     }) |>
         purrr::discard(is.na)
-
+    
+    nodLab <- match.arg(
+        arg = showNodeLabels,
+        choices = c("p", "c", "o", "f", "g"),
+        several.ok = TRUE
+    )
+    nodLabRgx <- paste0("[", paste0(nodLab, collapse = ""), "]__")
     treeData <- dat |>
         dplyr::mutate(
             showNodeLabs = dplyr::case_when(
-                grepl("[p]__", features) ~ features,
+                grepl(nodLabRgx, features) ~ features,
                 TRUE ~ NA
             )
         )
+    # return(treeData)
+    
     gt <- ggtree::ggtree(
         tree, layout = "circular",  branch.length = "none", size = 0.2
     ) %<+% treeData
+    
+    if (showTipLabels) {
+        gt <- gt + 
+            ggtree::geom_tiplab(
+                mapping = ggtree::aes(label = features), size = 2,
+                geom = "text", na.rm=TRUE
+            )
+    }
+    
     gt2 <- gt +
-        ggtree::geom_tiplab(
-            mapping = ggtree::aes(label = features), size = 2, geom = "text"
-        ) +
         ggtree::geom_tippoint(
-            mapping = ggtree::aes(fill = sample, size = abs), shape = 21
+            mapping = ggtree::aes(fill = sample, size = abs), shape = 21,
+            na.rm=TRUE
         ) +
         ggtree::geom_nodepoint(
-            mapping = ggtree::aes(fill = sample, size = abs), shape = 21
+            mapping = ggtree::aes(fill = sample, size = abs), shape = 21,
+            na.rm = TRUE
         ) +
         ggrepel::geom_label_repel(
-            mapping = ggtree::aes(label = showNodeLabs)
+            mapping = ggtree::aes(label = showNodeLabs),
+            na.rm = TRUE
         ) +
-        ggtree::theme(legend.position = "right") +
         ggtree::scale_fill_manual(
-            values = c("red", "forestgreen"), breaks = c(control, case)
-        )
-
+            values = colors, breaks = c(controlVar, caseVar),
+            name = "Sample", na.value = NA
+        ) +
+        ggplot2::scale_size(name = "Absolute\nscore") +
+        ggtree::theme(legend.position = "right")
     for (i in collapseThem) {
-        gt2 <- ggtree::collapse(gt2, node = i)
+        gt2 <- withCallingHandlers(
+            ggtree::collapse(gt2, node = i),
+            warning = function(w) {
+                if (grepl("collapse", w$message)) {
+                    invokeRestart("muffleWarning")
+                }
+            }
+        )
     }
-    gt2
-}
-
-
-.cladPlotTips <- function() {
-
-}
-
-.cladPlotAll <- function() {
-
+    return(gt2)
 }
 
 # Run lefser at all taxonomic levels --------------------------------------
@@ -89,19 +135,32 @@ lefserPlotClad <- function(x) {
 #' @export
 #'
 lefserAllRanks <- function(relab,...) {
+    ## Feature names should have the full taxonomy
     se <- .rowNames2RowData(relab)
     seL <- mia::splitByRanks(se)
     ## The kingdom level is not needed
     ## The mia package doesn't support strain.
     seL <- seL[names(seL) != "kingdom"]
+    seL <- purrr::map(seL, ~ {
+        seVar <- .x
+        rowDat <- as.data.frame(SummarizedExperiment::rowData(seVar))
+        rowDat <- purrr::discard(rowDat, function(x) all(is.na(x)))
+        rowDat <- S4Vectors::DataFrame(rowDat)
+        SummarizedExperiment::rowData(seVar) <- rowDat
+        seVar
+    })
+    for (i in seq_along(seL)) {
+        rownames(seL[[i]]) <- .lognRowNames(seL[[i]])
+    }
+    
     res <- seL |>
         purrr::map(function(x, ...) lefser(relab = x,...), ...) |>
         dplyr::bind_rows()
     resOriginal <- lefser(relab, ...)
     ## Get only tip names (full names with full taxonomy are too long).
-    resOriginal$features  <- stringr::str_extract(
-        resOriginal$features, "[^|]+$"
-    )
+    # resOriginal$features  <- stringr::str_extract(
+    #     resOriginal$features, "[^|]+$"
+    # )
     res <- res |>
         ## Avoid repeating features.
         dplyr::filter(!.data[["features"]] %in% resOriginal$features) |>
@@ -111,30 +170,16 @@ lefserAllRanks <- function(relab,...) {
     controlVar <- attr(resOriginal, "lgroupf")
     caseVar <- attr(resOriginal, "case")
 
-    ## This code could be added to the plotting function
-    ## To avoid modfying the current lefser function
-    resOutput <- res |>
-        dplyr::mutate(
-            sample = dplyr::case_when(
-                ## This assumes positive values always mean enriched in
-                ## the case condition.
-                .data[["scores"]] > 0 ~ .env[["caseVar"]],
-                TRUE ~ .env[["controlVar"]]
-            )
-        ) |>
-        dplyr::mutate(abs = abs(.data[["scores"]])) |>
-        as.data.frame()
-
-    class(resOutput) <- c("lefser_df_all", class(resOutput))
+    class(res) <- c("lefser_df_all", class(res))
 
     ## These pathStrings could be used in the plotting function instead (or not)
     pathStrings <- .selectPathStrings(relab, res)
-    attr(resOutput, "pathStrings") <- pathStrings
-    attr(resOutput, "tree") <- .toTree(pathStrings)
+    attr(res, "pathStrings") <- pathStrings
+    attr(res, "tree") <- .toTree(pathStrings)
 
-    attr(resOutput, "lgroupf") <- controlVar
-    attr(resOutput, "case") <- caseVar
-    return(resOutput)
+    attr(res, "lgroupf") <- controlVar
+    attr(res, "case") <- caseVar
+    return(res)
 }
 
 ## Add taxonomic information to rowData
@@ -228,4 +273,29 @@ lefserAllRanks <- function(relab,...) {
         from = chr_vct[1:length(chr_vct)-1],
         to = chr_vct[2:length(chr_vct)]
     )
+}
+
+## This function extracts only the last element of the taxonomy
+.extracTips <- function(pathStrs) {
+    stringr::str_extract(pathStrs, "[^|]+$")
+}
+
+
+# Utils -------------------------------------------------------------------
+.lognRowNames <- function(se) {
+    dat <- SummarizedExperiment::rowData(se) |> 
+        as.data.frame() |> 
+        tibble::rownames_to_column(var = "rowname") |> 
+        dplyr::relocate(.data[["rowname"]])
+    lastColLgl <- all(dat[[colnames(dat)[ncol(dat)]]] == dat[["rowname"]])
+    if (lastColLgl) {
+        dat <- dat[, which(colnames(dat) != "rowname")]
+        output <- dat |> 
+            tidyr::unite(
+                col = "features", 1:tidyselect::last_col(), 
+                sep = "|", remove = TRUE, 
+            ) |> 
+            dplyr::pull(.data[["features"]])
+    } 
+    return(output)
 }
