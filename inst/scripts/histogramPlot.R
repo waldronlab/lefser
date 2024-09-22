90c9d2d80c5e2c5397f43785f53a7c60f5430519
asyakhl <asyakhleb@gmail.com>
2024-09-22 15:14:46 -0500

    resolving merge conflict for inst/scripts/histogramPlot.R

--- inst/scripts/histogramPlot.R
+++ inst/scripts/histogramPlot.R
@@ -5,6 +5,19 @@ tn <- get_terminal_nodes(rownames(zeller14))
 zeller14tn <- zeller14[tn,]
 zeller14tn_ra <- relativeAb(zeller14tn)
 
+res <- lefser(
+    zeller14tn_ra, classCol = "study_condition", subclassCol = "age_category"
+)
+lefserPlot(res)
+lefserPlot(res, colors = "l")
+lefserPlot(res, colors = "g")
+lefserPlot(res, colors = c("purple", "pink"))
+sessioninfo::session_info()
+
+
+
+
+
 # (1) Using classes only
 res_class <- lefser(zeller14tn_ra,
                     classCol = "study_condition")
@@ -12,7 +25,17 @@ res_class <- lefser(zeller14tn_ra,
 res_subclass <- lefser(zeller14tn_ra,
                     classCol = "study_condition",
                     subclassCol = "age_category")
-plot_class <- lefsePlotFeat(res_class, res_class$features[[1]])
-plot_class
-plot_subclass <- lefsePlotFeat(res_subclass, res_subclass$features[[2]])
+
+plot_class <- lefsePlotFeat(res_class, res_class$features[[1]], colors = "g")
+plot_subclass <- lefsePlotFeat(res_subclass, res_subclass$features[[2]], colors = "g")
+
+plot_class <- lefsePlotFeat(res_class, res_class$features[[1]], colors = c("orange", "blue"))
+plot_subclass <- lefsePlotFeat(res_subclass, res_subclass$features[[1]], colors = c("orange", "blue"))
 plot_subclass
+
+lefserPlot(res_subclass, colors = "")
+
+
+
+
+
