e122ebb560a5eea00abc0606f0e68d18fa5d8233
sdgamboa <samuel.gamboa.tuz@gmail.com>
2024-09-30 15:10:47 -0400

    Update varialbe name of the result in unit test for lefserPlotClad

--- tests/testthat/test-lefserPlotClad.R
+++ tests/testthat/test-lefserPlotClad.R
@@ -5,14 +5,15 @@ z14tn <- z14[tn, ]
 z14tn_ra <- relativeAb(z14tn)
 
 res <- lefser(z14tn_ra, classCol = "study_condition")
+
 z14_input <- rowNames2RowData(z14tn_ra)
-resAll <- lefserClades(z14_input, classCol = "study_condition")
+resClades <- lefserClades(z14_input, classCol = "study_condition")
 
 test_that("lefserClades works", {
-    expect_s3_class(resAll, "lefser_df_clades")
+    expect_s3_class(resClades, "lefser_df_clades")
 })
 
 test_that("lefserPlotClad works", {
-    ggt <- lefserPlotClad(df = resAll)
+    ggt <- lefserPlotClad(df = resClades)
     expect_s3_class(ggt, "ggtree")
 })
