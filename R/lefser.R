447161c640222da12697a44d4e843c5a9b7b1d45
LiNk-NY <marcel.ramos@sph.cuny.edu>
2025-11-14 13:46:02 -0500

    fix formatting lefser.R
    - clean up spacing in example

--- R/lefser.R
+++ R/lefser.R
@@ -1,81 +1,88 @@
 wilcox_pstats <- function(datamat, class, index) {
-  apply(datamat, 1L, function(values) {
-    wx <- suppressWarnings({
-        coin::wilcox_test(values ~ class, subset = index)
+    apply(datamat, 1L, function(values) {
+        wx <- suppressWarnings({
+            coin::wilcox_test(values ~ class, subset = index)
+        })
+        c(pvalue = coin::pvalue(wx), statistic = coin::statistic(wx))
     })
-    c(pvalue = coin::pvalue(wx), statistic = coin::statistic(wx))
-  })
 }
 
 ## Wilcoxon Rank-Sum Test for the sub-classes
-fillPmatZmat <- function(class,
-                         subclass,
-                         relab_sub,
-                         p.threshold,
-                         method)
-{
-  if (!nrow(relab_sub))
-    return(relab_sub)
-  # creates a list of boolean vectors, each vector indicates
-  # existence (TRUE) or absence (FALSE) of a class/sub-class combination
-  combos <- apply(
-    expand.grid(levels(class), levels(subclass)), 1L, paste0, collapse = "."
-  )
-  combined <- interaction(class, subclass)
-  whichlist <- lapply(
-    setNames(nm = sort(combos)), function(combo) which(combo == combined)
-  )
+fillPmatZmat <- function(class, subclass, relab_sub, p.threshold, method) {
+    if (!nrow(relab_sub)) return(relab_sub)
+    # creates a list of boolean vectors, each vector indicates
+    # existence (TRUE) or absence (FALSE) of a class/sub-class combination
+    combos <- apply(
+        expand.grid(levels(class), levels(subclass)),
+        1L,
+        paste0,
+        collapse = "."
+    )
+    combined <- interaction(class, subclass)
+    whichlist <- lapply(
+        setNames(nm = sort(combos)),
+        function(combo) which(combo == combined)
+    )
 
-  ## uses Wilcoxon rank-sum test to test for significant differential abundances between
-  ## subclasses of one class against subclasses of all other classes; results are saved in
-  ## "pval_mat" and "z_mat" matrices
-  ssubclass <- seq_along(levels(subclass))
-  iters <- expand.grid(ssubclass, ssubclass + length(ssubclass))
-  inds <- apply(iters, 1L, function(x) unlist(whichlist[x], use.names = FALSE))
+    ## uses Wilcoxon rank-sum test to test for significant differential abundances between
+    ## subclasses of one class against subclasses of all other classes; results are saved in
+    ## "pval_mat" and "z_mat" matrices
+    ssubclass <- seq_along(levels(subclass))
+    iters <- expand.grid(ssubclass, ssubclass + length(ssubclass))
+    inds <- apply(
+        iters,
+        1L,
+        function(x) unlist(whichlist[x], use.names = FALSE)
+    )
 
-  z_mat <- pval_mat <- matrix(
-    NA, nrow = nrow(relab_sub), ncol = length(inds),
-    dimnames = list(rownames(relab_sub), NULL)
-  )
-  for (i in seq_along(inds)) {
-    results <- wilcox_pstats(relab_sub, class = class, index = inds[[i]])
-    pval_mat[, i] <- stats::p.adjust(results["pvalue", ], method = method)
-    z_mat[, i] <- results["statistic", ]
-  }
+    z_mat <- pval_mat <- matrix(
+        NA,
+        nrow = nrow(relab_sub),
+        ncol = length(inds),
+        dimnames = list(rownames(relab_sub), NULL)
+    )
+    for (i in seq_along(inds)) {
+        results <- wilcox_pstats(relab_sub, class = class, index = inds[[i]])
+        pval_mat[, i] <- stats::p.adjust(results["pvalue", ], method = method)
+        z_mat[, i] <- results["statistic", ]
+    }
 
-  ## converts "pval_mat" into boolean matrix "logical_pval_mat" where
-  ## p-values <= wilcoxon.threshold
-  logical_pval_mat <- !is.na(pval_mat) & pval_mat <= p.threshold * 2.0
+    ## converts "pval_mat" into boolean matrix "logical_pval_mat" where
+    ## p-values <= wilcoxon.threshold
+    logical_pval_mat <- !is.na(pval_mat) & pval_mat <= p.threshold * 2.0
 
-  ## determines which rows (features) have all p-values<=0.05
-  ## and selects such rows from the matrix of z-statistics
-  sub <- rowSums(logical_pval_mat) == ncol(logical_pval_mat)
-  z_mat_sub <- z_mat[sub, , drop = FALSE]
-  ## confirms that z-statistics of a row all have the same sign
-  sub <- abs(rowSums(z_mat_sub)) == rowSums(abs(z_mat_sub))
-  relab_sub[names(sub[sub]), , drop = FALSE]
+    ## determines which rows (features) have all p-values<=0.05
+    ## and selects such rows from the matrix of z-statistics
+    sub <- rowSums(logical_pval_mat) == ncol(logical_pval_mat)
+    z_mat_sub <- z_mat[sub, , drop = FALSE]
+    ## confirms that z-statistics of a row all have the same sign
+    sub <- abs(rowSums(z_mat_sub)) == rowSums(abs(z_mat_sub))
+    relab_sub[names(sub[sub]), , drop = FALSE]
 }
 
 ## ensures that more than half of the values in each for each feature are unique
 ## if that is not the case then a count value is altered by adding it to a small value
 ## generated via normal distribution with mean=0 and sd=5% of the count value
-createUniqueValues <- function(df, class){
-  orderedrows <- rownames(df)
-  splitdf <- split(df, class)
-  maxim <- vapply(table(class), function(x) max(x * 0.5, 4), numeric(1L))
-  for (i in seq_along(splitdf)) {
-    sdat <- splitdf[[i]]
-    splitdf[[i]][] <- lapply(sdat, function(cols) {
-        if (length(unique(cols)) > maxim[i])
-            cols
-        else
-            abs(cols + rnorm(
-                length(cols), mean = 0, sd = max(cols * 0.05, 0.01))
-            )
-    })
-  }
-  df <- do.call(rbind, unname(splitdf))
-  df[match(orderedrows, rownames(df)),, drop = FALSE]
+createUniqueValues <- function(df, class) {
+    orderedrows <- rownames(df)
+    splitdf <- split(df, class)
+    maxim <- vapply(table(class), function(x) max(x * 0.5, 4), numeric(1L))
+    for (i in seq_along(splitdf)) {
+        sdat <- splitdf[[i]]
+        splitdf[[i]][] <- lapply(sdat, function(cols) {
+            if (length(unique(cols)) > maxim[i]) cols else
+                abs(
+                    cols +
+                        rnorm(
+                            length(cols),
+                            mean = 0,
+                            sd = max(cols * 0.05, 0.01)
+                        )
+                )
+        })
+    }
+    df <- do.call(rbind, unname(splitdf))
+    df[match(orderedrows, rownames(df)), , drop = FALSE]
 }
 
 
@@ -89,14 +96,13 @@ createUniqueValues <- function(df, class){
 #'
 #' @noRd
 #' @keywords internal
-ldaFunction <- function (data, classes) {
-
+ldaFunction <- function(data, classes) {
     ## Fitting LDA model
     lda.fit <- lda(class ~ ., data = data)
     w <- lda.fit$scaling[, 1] # extract LDA coefficients
-    w.unit <- w / sqrt(sum(w ^ 2)) # scaling LDA coefficient by their Euclidean norm to get unit-normalized coefficient
+    w.unit <- w / sqrt(sum(w^2)) # scaling LDA coefficient by their Euclidean norm to get unit-normalized coefficient
 
-    ss <- data[,-match("class", colnames(data)), drop = FALSE]
+    ss <- data[, -match("class", colnames(data)), drop = FALSE]
     xy.matrix <- as.matrix(ss) # the original feature matrix
 
     ## Transform the original feature matrix
@@ -116,13 +122,13 @@ ldaFunction <- function (data, classes) {
     for (v in seq_along(scal)) {
         if (!is.na(scal[v])) {
             coeff[v] <- abs(scal[v])
-        } else{
+        } else {
             coeff[v] <- 0
         }
     }
 
     ## Feature Importance
-    lda.means.diff <- (lda.fit$means[2,] - lda.fit$means[1,]) # difference between the class means for each feature
+    lda.means.diff <- (lda.fit$means[2, ] - lda.fit$means[1, ]) # difference between the class means for each feature
     res <- (lda.means.diff + coeff) / 2
     names(res) <- colnames(lda.fit$means)
 
@@ -132,21 +138,21 @@ ldaFunction <- function (data, classes) {
 
 ## Kruskal-Wallis Rank Sum Test for the classes
 filterKruskal <- function(relab, class, p.value, method = method) {
-  # applies "kruskal.test.alt" function to each row (feature) of relab
-  # to detect differential abundance between classes, 0 and 1
-  kw.res <- apply(relab, 1L, function(x) {
-    kruskal.test(x ~ class)[["p.value"]]
-  })
-  # TRUE for p-values less than or equal to kw.threshold
-  kw.res <- stats::p.adjust(kw.res, method = method)
-  kw.sub <- kw.res < p.value
+    # applies "kruskal.test.alt" function to each row (feature) of relab
+    # to detect differential abundance between classes, 0 and 1
+    kw.res <- apply(relab, 1L, function(x) {
+        kruskal.test(x ~ class)[["p.value"]]
+    })
+    # TRUE for p-values less than or equal to kw.threshold
+    kw.res <- stats::p.adjust(kw.res, method = method)
+    kw.sub <- kw.res < p.value
 
-  # NAs are FALSE
-  kw.sub[is.na(kw.sub)] <- FALSE
+    # NAs are FALSE
+    kw.sub[is.na(kw.sub)] <- FALSE
 
-  # extracts features with statistically significant differential abundance
-  # from "relab" matrix
-  relab[kw.sub,, drop = FALSE]
+    # extracts features with statistically significant differential abundance
+    # from "relab" matrix
+    relab[kw.sub, , drop = FALSE]
 }
 
 #' R implementation of the LEfSe method
@@ -209,42 +215,49 @@ filterKruskal <- function(relab, class, p.value, method = method) {
 #' @import SummarizedExperiment
 #'
 #' @examples
+#' data(zeller14)
+#' zeller14 <- zeller14[, zeller14$study_condition != "adenoma"]
+#' tn <- get_terminal_nodes(rownames(zeller14))
+#' zeller14tn <- zeller14[tn,]
+#' zeller14tn_ra <- relativeAb(zeller14tn)
 #'
-#'     data(zeller14)
-#'     zeller14 <- zeller14[, zeller14$study_condition != "adenoma"]
-#'     tn <- get_terminal_nodes(rownames(zeller14))
-#'     zeller14tn <- zeller14[tn,]
-#'     zeller14tn_ra <- relativeAb(zeller14tn)
+#' # (1) Using classes only
+#' res_class <- lefser(relab = zeller14tn_ra, classCol = "study_condition")
 #'
-#'     # (1) Using classes only
-#'     res_class <- lefser(zeller14tn_ra,
-#'                         classCol = "study_condition")
-#'     # (2) Using classes and sub-classes
-#'     res_subclass <- lefser(zeller14tn_ra,
-#'                         classCol = "study_condition",
-#'                         subclassCol = "age_category")
-#'
+#' # (2) Using classes and sub-classes
+#' res_subclass <- lefser(
+#'     relab = zeller14tn_ra,
+#'     classCol = "study_condition",
+#'     subclassCol = "age_category"
+#' )
 #' @export
-lefser <-
-  function(relab,
-           kruskal.threshold = 0.05,
-           wilcox.threshold = 0.05,
-           lda.threshold = 2.0,
-           classCol = "CLASS",
-           subclassCol = NULL,
-           assay = 1L,
-           trim.names = FALSE,
-           checkAbundances = TRUE,
-           method = "none",
-           ...
+lefser <- function(
+    relab,
+    kruskal.threshold = 0.05,
+    wilcox.threshold = 0.05,
+    lda.threshold = 2.0,
+    classCol = "CLASS",
+    subclassCol = NULL,
+    assay = 1L,
+    trim.names = FALSE,
+    checkAbundances = TRUE,
+    method = "none",
+    ...
 ) {
     relab_data <- assay(relab, i = assay)
 
     ## Check whether relative abundance is provided or not
-    if (checkAbundances && !identical(all.equal(colSums(relab_data),
-                                                rep(1e6, ncol(relab_data)),
-                                                check.attributes = FALSE),
-                                      TRUE)) {
+    if (
+        checkAbundances &&
+            !identical(
+                all.equal(
+                    colSums(relab_data),
+                    rep(1e6, ncol(relab_data)),
+                    check.attributes = FALSE
+                ),
+                TRUE
+            )
+    ) {
         warning("Convert counts to relative abundances with 'relativeAb()'")
     }
 
@@ -257,32 +270,38 @@ lefser <-
         stop(msg) # ensure the class has only two levels
     }
     message(
-        "The outcome variable is specified as '", classCol,
-        "' and the reference category is '", lclassf[1],
+        "The outcome variable is specified as '",
+        classCol,
+        "' and the reference category is '",
+        lclassf[1],
         "'.\n See `?factor` or `?relevel` to change the reference category."
     )
 
     ## Kruskal-Wallis Rank Sum Test for the classes
-    relab_sub <- filterKruskal(relab = relab_data,
-                               class = classf,
-                               p.value = kruskal.threshold,
-                               method = method)
+    relab_sub <- filterKruskal(
+        relab = relab_data,
+        class = classf,
+        p.value = kruskal.threshold,
+        method = method
+    )
 
     ## Wilcoxon Rank-Sum Test for the sub-classes
     if (!is.null(subclassCol)) {
         subclass <- as.factor(colData(relab)[[subclassCol]])
         subclass <- droplevels(subclass)
         ## z-statistics result of the features passing the significance cut-off
-        relab_sub <- fillPmatZmat(class = classf,
-                                  subclass = subclass,
-                                  relab_sub = relab_sub,
-                                  p.threshold = wilcox.threshold,
-                                  method = method)
+        relab_sub <- fillPmatZmat(
+            class = classf,
+            subclass = subclass,
+            relab_sub = relab_sub,
+            p.threshold = wilcox.threshold,
+            method = method
+        )
     }
 
     ## Return an empty data table if there is no significant features
-    if(nrow(relab_sub) == 0L){
-      return(.return_no_results())
+    if (nrow(relab_sub) == 0L) {
+        return(.return_no_results())
     }
 
     ## Transposed relative abundance matrix with the 'class' column
@@ -304,12 +323,14 @@ lefser <-
     }
     ## Processing LDA scores
     processed_scores <-
-      sign(raw_lda_scores) * log((1 + abs(raw_lda_scores)), 10)
-    processed_sorted_scores <- sort(processed_scores)   # sorting of scores
-    scores_df <- data.frame(features = names(processed_sorted_scores),
-                            scores = as.vector(processed_sorted_scores),
-                            stringsAsFactors = FALSE)
-    scores_df <- .trunc(scores_df, trim.names)   # short-form of taxa name
+        sign(raw_lda_scores) * log((1 + abs(raw_lda_scores)), 10)
+    processed_sorted_scores <- sort(processed_scores) # sorting of scores
+    scores_df <- data.frame(
+        features = names(processed_sorted_scores),
+        scores = as.vector(processed_sorted_scores),
+        stringsAsFactors = FALSE
+    )
+    scores_df <- .trunc(scores_df, trim.names) # short-form of taxa name
 
     ## Filter with LDA threshold
     threshold_scores <- abs(scores_df$scores) >= lda.threshold
@@ -323,7 +344,7 @@ lefser <-
     ## Add attributes with argument values
     ## This is used for plottin functions
     attr(res_scores, "inputSE") <- relab
-    attr(res_scores, "kth") <-  kruskal.threshold
+    attr(res_scores, "kth") <- kruskal.threshold
     attr(res_scores, "wth") <- wilcox.threshold
     attr(res_scores, "ldath") <- lda.threshold
     attr(res_scores, "class_arg") <- classCol
@@ -340,7 +361,7 @@ lefser <-
     attr(res_scores, "case") <- lclassf[2]
 
     res_scores
- }
+}
 
 .return_no_results <- function() {
     message("No significant features found.")
